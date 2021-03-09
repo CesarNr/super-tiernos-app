2ec76b6bd67dee40771d794cc3045835db5eb5f0
CesarNr <cesar.inuncira@gmail.com>
2021-03-09 09:20:13 -0500

    gishare code on github

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+/* import Product from '../components/Product'; */
+import Rating from '../components/Rating';
+import data from '../data';
+
+export default function ProductScreen(props) {
+    const product = data.products.find(x => x._id === props.match.params.id);
+    if (!product) {
+        return <div> Product not found.</div>;
+    }
+    return (
+        <div>
+            <Link to="/">Regresar</Link>
+            <div className="row top">
+                <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name}></img>
+                </div>
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>
+                                {product.name}
+                            </h1>
+                        </li>
+                        <li>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={product.numReviews}
+                            ></Rating>
+                        </li>
+                        <li>
+                            Precio: ${product.price}
+                        </li>
+                        <li>
+                            Descripción:
+                            <p> {product.description} </p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-1">
+                    <div className="card card-body">
+                        <ul>
+                            <li>
+                                <div className="row">
+                                    <div>Precio: </div>
+                                    <div className="price">${product.price} </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="row">
+                                    <div>Estado: </div>
+                                    <div>
+                                        {product.countInStore > 0 ? (
+                                            <span className="success">Disponible</span>
+                                        ) : (
+                                                <span className="danger">No disponible</span>
+                                            )}
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Agregar al carro</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
