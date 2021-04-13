a88d987fbaf9138f1d302c4445b15785268146bb
CesarNr <cesar.inuncira@gmail.com>
2021-04-13 11:28:56 -0500

    Fazt update 5

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import data from './data.js';
+import path from 'path';
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.get('/api/products', (req, res) => {
 
 // Configuración para Heroku
 const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
