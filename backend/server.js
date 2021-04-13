d814a2a1477b7423e7d01cf4ee160e788e65ea70
CesarNr <cesar.inuncira@gmail.com>
2021-04-12 23:06:11 -0500

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ app.listen(port, () => {
 
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
